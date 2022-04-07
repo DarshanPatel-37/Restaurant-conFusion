7d51934855909d3d8657cad37a68354e29a9726d
DarshanPatel-37 <darshanp2019@gmail.com>
2022-04-07 22:07:39 +0530

    updated version

---
+++ conFusion-Server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '943331503006158',
+        clientSecret: 'fe9f1bd3751143385f79b9b8c7d324c6'
+    }
+}
